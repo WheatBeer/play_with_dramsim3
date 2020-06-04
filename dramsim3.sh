638c3d70bb43cf9f86ca5dd1a13a4de5ad2721ed
WheatBeer <sungminryu94@gmail.com>
2020-06-04 22:34:49 +0900

    first commit

--- dramsim3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ext
-# Clone DRAMsim3 master branch
-git clone https://github.com/umd-memsys/DRAMsim3.git
-cd DRAMsim3
-# Make a DRAMsim3 library(Shared)
-make libdramsim3.so
-#make all 
---
+++ gem5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ext
+# Clone gem5 master branch
+git clone https://gem5.googlesource.com/public/gem5
+cd gem5
+# Make a gem5 library(Shared)
+#make all 
