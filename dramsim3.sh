3748a58223562160a4bbaf583c93a59078147ae2
WheatBeer <sungminryu94@gmail.com>
2020-06-18 00:55:34 +0900

    first commit

--- dramsim3.sh
+++ dramsim3.sh
@@ -3,8 +3,7 @@
 cd ext
 # Clone DRAMsim3 master branch
 git clone https://github.com/umd-memsys/DRAMsim3.git
-cp configuration.cc ./DRAMsim3/src
+cp configuration_for_dramsim3.cc ./DRAMsim3/src
 cd DRAMsim3
 # Make a DRAMsim3 library(Shared)
 make libdramsim3.so
-#make all 
---
+++ ext/unroll_test_for_gem5/origin.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "layer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Loop Unrolling Test(Original)!" << endl;
+	layer_t alex_conv_1(227, 227, 3, 11, 11, 96, 4, 1);
+	layer_t &layer = alex_conv_1;
+	
+	//layer.print_stats();
+
+	cout << "Convolutional Layer Execution Start!" << endl;
+	for(unsigned k = 0; k < layer.K; k++) {
+		for(unsigned oh = 0; oh < layer.OH; oh++) {
+			for(unsigned ow = 0; ow < layer.OW; ow++) {
+				for(unsigned c = 0; c < layer.C; c++) {
+					for(unsigned r = 0; r < layer.R; r++) {
+						for(unsigned s = 0; s < layer.S; s++) {
+							unsigned h = oh * layer.stride + r;
+							unsigned w = ow * layer.stride + s;
+							*layer.output(ow, oh, k) += layer.input(w, h, c) * layer.filter(s, r, c, k);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	cout << "Convolutional Layer Execution Finish!" << endl;
+	//layer.print_output();
+
+	return 0;
+}
